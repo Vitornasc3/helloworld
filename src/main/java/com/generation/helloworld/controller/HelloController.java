03fb675640d5c4dd6bd03edf9ff63affd56bf7a3
Vitor Ferreira <vitornasc673@gmail.com>
2023-12-07 12:08:06 -0300

    Projeto hello world

---
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.generation.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@GetMapping
+	public String hello() {
+		return "<h2><b>Hello turma 68!</b></h2>";
+	}
+	
+	
+	@GetMapping("/top")
+	public String top() {
+		return "<h1><b>Turma 68 é topppp!</b>";
+	}
+
+}
